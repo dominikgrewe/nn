e6d09cb90c40dc6007f72708d177d4d5b7aa386a
Dominik Grewe <dominikg@google.com>
2015-04-16 14:23:47 +0100

    Implement Min and Max with torch.min/max and torch.indexedWrite.

--- Max.lua
+++ Max.lua
@@ -4,13 +4,20 @@ function Max:__init(dimension)
    parent.__init(self)
    dimension = dimension or 1
    self.dimension = dimension
-   self.indices = torch.Tensor()
+   self.indices = torch.LongTensor()
 end
 
 function Max:updateOutput(input)
-   return input.nn.Max_updateOutput(self, input)
+   torch.max(self.output, self.indices, input, self.dimension)
+   if self.output:dim() > 1 then
+      self.output = self.output:select(self.dimension, 1)
+   end
+   return self.output
 end
 
 function Max:updateGradInput(input, gradOutput)
-   return input.nn.Max_updateGradInput(self, input, gradOutput)
+   gradOutput = gradOutput:viewAs(self.indices)
+   self.gradInput:resizeAs(input):zero()
+   self.gradInput:indexedWrite(self.dimension, self.indices, gradOutput)
+   return self.gradInput
 end
--- Min.lua
+++ Min.lua
@@ -4,13 +4,20 @@ function Min:__init(dimension)
    parent.__init(self)
    dimension = dimension or 1
    self.dimension = dimension
-   self.indices = torch.Tensor()
+   self.indices = torch.LongTensor()
 end
 
 function Min:updateOutput(input)
-   return input.nn.Min_updateOutput(self, input)
+   torch.min(self.output, self.indices, input, self.dimension)
+   if self.output:dim() > 1 then
+      self.output = self.output:select(self.dimension, 1)
+   end
+   return self.output
 end
 
 function Min:updateGradInput(input, gradOutput)
-   return input.nn.Min_updateGradInput(self, input, gradOutput)
+   gradOutput = gradOutput:viewAs(self.indices)
+   self.gradInput:resizeAs(input):zero()
+   self.gradInput:indexedWrite(self.dimension, self.indices, gradOutput)
+   return self.gradInput
 end
